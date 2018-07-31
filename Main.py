1804f3ae571d93998f87cd72afa8c2b16d26744f
sandyvaslon <36742319+sandyvaslon@users.noreply.github.com>
2018-07-31 23:41:28 +0200

    V2

--- Main.py
+++ Main.py
@@ -6,9 +6,9 @@ Created on Mon Jul 30 23:19:02 2018
 """
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+#from sklearn.cross_validation import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.model_selection import train_test_split
 
 rep=r'D:\Projets\Home_credit\data'
 app_df=pd.read_csv(rep + r'\application_train.csv')
@@ -28,10 +28,13 @@ for elem in app_df.columns:
     
 train, test = train_test_split(app_df, test_size=0.2)
 #    
-#y=train['TARGET']
-#X=train.drop(['TARGET'], axis=1)
+y=train['TARGET']
+X=train.drop(['TARGET'], axis=1)
 
 del app_df
 
 clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(train['TARGET'], train.drop(['TARGET'], axis=1))
+clf.fit(X, y)
+
+X=test.drop(['TARGET'], axis=1)
+pred=clf.predict(X)
